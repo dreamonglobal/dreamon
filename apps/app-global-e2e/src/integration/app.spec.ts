ddb05bb60e21fd34f2effa9a240b8a9011a8f153
Jordan <jordan@jpdesigning.com>
2021-11-02 10:15:38 -0400

    ci: add pull-request ci script

--- apps/app-global-e2e/src/integration/app.spec.ts
+++ apps/app-global-e2e/src/integration/app.spec.ts
@@ -1,13 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('app-global', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/'))
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to app-global!');
-  });
-});
+  it('should load home page', () => {
+    cy.get('h1').contains('Dream On: Honduras 2021')
+  })
+})
